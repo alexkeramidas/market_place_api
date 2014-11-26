d8f245220e6d49a4bb06ee734114d4d450a6c690
Alexandros Keramidas <dev.alexkeramidas@gmail.com>
2014-11-26 19:55:30 +0200

    Chapter 3 Finished

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ require 'api_constraints'
 MarketPlaceApi::Application.routes.draw do
   devise_for :users
   # Api definition
-  namespace :api, defaults: {format: :json}, constraints: {subdomain: 'api'}, path: '/' do
+  namespace :api, defaults: {format: :json}, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, :only => [:show, :create, :update, :destroy]
 
